620fa8dce6a83caf00479887195babfbee45f773
Sarah Smith <155038730+sarahlibx@users.noreply.github.com>
2025-01-16 19:24:39 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const instaColor = document.querySelector(".fa-instagram");
+instaColor.style.color = "#d45bb9";
+
+const facebookColor = document.querySelector(".fa-facebook");
+facebookColor.style.color = "#6a99b5";
+
+const openingHours = document.querySelector(".contact p");
+openingHours.style.border = "1px solid #d45bb9";
+openingHours.style.padding = "1em";
